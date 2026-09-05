1842803f3298b69aa33f4a5ae123ba0a1ae85478
auyondebnath-cpu <auyon.debnath@students.mq.edu.au>
2026-09-05 23:26:38 +1000

    Add entities list and setEntities() to SimulationPanel

--- predatorPrey/src/SimulationPanel.java
+++ predatorPrey/src/SimulationPanel.java
@@ -1,7 +1,15 @@
 import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JPanel;
 
 public class SimulationPanel extends JPanel{
+    private List<Entity> entities = new ArrayList<>();
+
+    public void setEntities(List<Entity> entities){
+        this.entities = entities;
+    }
+
     @Override
     protected void paintComponent(Graphics g){
         super.paintComponent(g);
