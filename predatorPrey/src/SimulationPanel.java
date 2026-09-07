5f595cc622c8f50c20dbb65ed39b7e09f6fa3501
auyondebnath-cpu <auyon.debnath@students.mq.edu.au>
2026-09-07 13:59:13 +1000

    Add basic entity rendering (colored ovals) to SimulationPanel

--- predatorPrey/src/SimulationPanel.java
+++ predatorPrey/src/SimulationPanel.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.awt.Graphics;
 import java.util.ArrayList;
 import java.util.List;
@@ -13,5 +14,18 @@ public class SimulationPanel extends JPanel{
     @Override
     protected void paintComponent(Graphics g){
         super.paintComponent(g);
+        for(Entity entity: entities){
+            if(entity instanceof Predator){
+                g.setColor(Color.RED);
+            } else if(entity instanceof Prey){
+                g.setColor(Color.BLUE);
+            } else if (entity instanceof Grass){
+                g.setColor(Color.GREEN);
+            } else{
+                g.setColor(Color.BLACK);
+            }
+
+            g.fillOval(entity.getX(), entity.getY(), 10, 10);
+        }
     }
 }
