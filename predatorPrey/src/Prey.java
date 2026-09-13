8f20a7553d1b09ef1826b2740d83c42ec69edb4f
auyondebnath-cpu <auyon.debnath@students.mq.edu.au>
2026-09-14 01:00:33 +1000

    Add flee state fields to Prey
    Introduced fleeTarget and a countdown timer so prey remember
    a threat for a short duration after detection, independent of
    whether the predator stays in range every tick.

--- predatorPrey/src/Prey.java
+++ predatorPrey/src/Prey.java
@@ -1,5 +1,9 @@
 public class Prey extends Creature {
     private Boolean inDanger;
+    private Entity fleeTarget;
+    private int fleeTicksRemaining = 0;
+    private static final int FLEE_DURATION_TICKS = 20;
+    private static final double FLEE_SPEED_MULTIPLIER = 1.8;
 
     public Prey(int speed, int hunger, boolean isFood, int x, int y) {
         super(speed, hunger, isFood, x, y);
@@ -9,13 +13,20 @@ public class Prey extends Creature {
     public boolean isInDanger(){
         return inDanger;
     }
-
-    public void setInDanger(boolean inDanger){
-        this.inDanger = inDanger;
-    }
    
     public void movement() {
 
+    }
+
+    public void setInDanger(boolean inDanger){
+        this.inDanger = inDanger;
+        if(inDanger){
+            fleeTicksRemaining = FLEE_DURATION_TICKS;
+        }
+    }
+
+    public void setFleeTarget(Entity fleeTarget){
+        this.fleeTarget = fleeTarget;
     }
 
     @Override
